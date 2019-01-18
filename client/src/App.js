6762498eec843264131839a3fac50ad68479fa6b
Daland Dixon <dalanddixon@Dalands-MBP.fios-router.home>
2019-01-18 15:05:43 -0500

    IT WORKSgit add -A

--- client/src/App.js
+++ client/src/App.js
@@ -1,124 +1,112 @@
-import React from 'react';
-import './App.css'
-import Wrapper from './components/Wrapper'
-import Navbar from './components/Navbar';
-import Gameboard from './components/Gameboard';
-import Tile from './components/Tile';
-import NumSpan from './components/NumSpan';
-import Footer from './components/Footer';
-import tiles from './tiles.json'
+import React, { Component } from "react";
+import "./App.css";
+import Wrapper from "./components/Wrapper";
+import Navbar from "./components/Navbar";
+import Gameboard from "./components/Gameboard";
+import Tile from "./components/Tile";
+import NumSpan from "./components/NumSpan";
+import Footer from "./components/Footer";
+import tiles from "./tiles.json";
 
-class App extends React.Component {
+class App extends Component {
   state = {
-    tiles: tiles.sort(function (a, b) { return 0.5 - Math.random() }),
+    tiles: tiles.sort(function(a, b) {
+      return 0.5 - Math.random();
+    }),
     emptySpaceIndex: tiles.findIndex(x => x.id === 0),
-    clickedTileIndex: null,
-    slidTileIndex: null,
-    switchable: false
-    // slidTileValue: null
+    clickedTileIndex: null
   };
 
   componentDidMount = () => {
-    console.log(`Empty Space Index: ${this.state.emptySpaceIndex}`)
-  }
+    console.log(`Empty space index on load: ${this.state.emptySpaceIndex}`);
+  };
 
-  tileClick = (clickedTileObject, emptySpaceObject) => {
+  handleTileClick = tile => {
+    if (tile.id > 0) {
+      this.setState(
+        {
+          clickedTileIndex: tiles.findIndex(x => x.id === tile.id)
+        },
+        () => {
+          const indexOne = this.state.emptySpaceIndex;
+          const indexTwo = this.state.clickedTileIndex;
+          const result = indexOne - indexTwo;
+          const test = () => {
+            console.log(`You clicked tile ${tile.value}!`);
+            console.log(`Clicked Tile Index: ${this.state.clickedTileIndex}`);
+            console.log(`These two can be switched!`);
+            this.swapArrayElements();
+          };
 
-    this.setState(
-      {
-        clickedTileIndex: tiles.findIndex(x => x.id === clickedTileObject.id)
-      }, () => {
-        // this.setState({ slidTileValue: tiles[this.state.clickedTileIndex].value }, ()=>{
-        //   console.log(`Slid Tile Value: ${this.state.slidTileValue}`)
-        // })
-        console.log(`Clicked Tile Index: ${this.state.clickedTileIndex}`);
-        // console.log(`Empty Space Object:\n{id: ${emptySpaceObject.id}, value: ${emptySpaceObject.value}}`)
-
-        const index1 = this.state.emptySpaceIndex;
-
-        const index2 = this.state.clickedTileIndex;
-
-        const result = index1 - index2;
-
-        // const slidValue = clickedTileObject.value;
-
-        const slideTile = () => {
-          // this.setState({ emptySpaceIndex: this.state.clickedTileIndex })
-          this.setState({ switchable: true, emptySpaceIndex: this.state.clickedTileIndex, slidTileIndex: this.state.clickedTileIndex + result }, () => {
-            console.log(`Slid Tile Index: ${this.state.slidTileIndex}`);
-            this.setState({ switchable: false });
-          });
-        }
-
-
-        if (result === 3 || result === -3) {
-          console.log(`Result equals ${result}. These two can be switched :)`);
-          slideTile()
-        }
-        else if (result === -1) {
-          if (index2 !== 3 && index2 !== 6) {
-            console.log(`Result equals ${result}. These two can be switched :)`);
-            slideTile()
+          if (result === 3 || result === -3) {
+            test();
+          } else if (result === -1) {
+            if (indexTwo !== 3 && indexTwo !== 6) {
+              test();
+            }
+          } else if (result === 1) {
+            if (indexTwo !== 2 && indexTwo !== 5) {
+              test();
+            }
           }
         }
-        else if (result === 1) {
-          if (index2 !== 2 && index2 !== 5) {
-            console.log(`Result equals ${result}. These two can be switched :)`);
-            slideTile()
-          }
-        }
-        else {
-          this.setState({ switchable: false })
-        }
-      })
-  }
+      );
+    }
+  };
 
-  chooseClass = (tile) => {
+  swapArrayElements = () => {
+    const tilesV = this.state.tiles;
+    const eSIV = this.state.emptySpaceIndex;
+    const cTIV = this.state.clickedTileIndex;
+
+    const temp = tilesV[eSIV];
+    tilesV[eSIV] = tilesV[cTIV];
+    tilesV[cTIV] = temp;
+
+    this.setState({ emptySpaceIndex: cTIV }, () => {
+      console.log(
+        `Clicked tile after set state: ${this.state.clickedTileIndex}`
+      );
+      console.log(`Empty Space Index: ${this.state.emptySpaceIndex}`);
+      console.log(this.state.tiles);
+    });
+  };
+
+  handleStyleClasses = tile => {
     const classes = [];
-    if (this.state.tiles.indexOf(tile) === this.state.emptySpaceIndex) {
+
+    if (tile.id === 0) {
       classes.length = 0;
       classes.push("empty-tile");
-      return classes
-    }
-    else {
+      return classes;
+    } else {
       classes.length = 0;
-      classes.push("tile")
-      return classes
+      classes.push("tile");
+      return classes;
     }
-  }
+  };
 
-  chooseValue = (tile) => {
-    if (this.state.tiles.indexOf(tile) === this.state.slidTileIndex && this.state.switchable === true) {
-      console.log(`Value to be switched: ${this.state.tiles[this.state.clickedTileIndex].value}`);
-      return this.state.tiles[this.state.clickedTileIndex].value
+  renderNumSpan = tile => {
+    if (tile.id === 0) {
+      return null;
+    } else {
+      return <NumSpan value={tile.value} />;
     }
-    else {
-      // console.log(`goodbye`)
-      return tile.value
-    }
-  }
+  };
 
   render() {
-
-
-
     return (
       <Wrapper>
         <Navbar />
         <Gameboard>
           {this.state.tiles.map(tile => (
-
             <Tile
-              id={tile.id}
               key={tile.id}
-              value={tile.value}
               tileObject={tile}
-              tileClick={this.tileClick}
-              class={this.chooseClass}
-              emptySpaceObject={this.state.tiles[this.state.emptySpaceIndex]}
+              class={this.handleStyleClasses(tile)}
+              handleTileClick={this.handleTileClick}
             >
-              {this.state.tiles.indexOf(tile) === this.state.emptySpaceIndex ? null
-                : <NumSpan value={this.chooseValue(tile)} />}
+              {this.renderNumSpan(tile)}
             </Tile>
           ))}
         </Gameboard>
--- client/src/components/Tile/Tile.js
+++ client/src/components/Tile/Tile.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React from "react";
 // import './Tile.css'
 
-const Tile = (props) => (
-  <div className={props.class(props.tileObject)} onClick={() => props.tileClick(props.tileObject, props.emptySpaceObject)}>
+const Tile = props => (
+  <div
+    className={props.class}
+    onClick={() => props.handleTileClick(props.tileObject)}
+  >
     {props.children}
   </div>
 );
+
+// ORIGINAL CODE ==================================
+// const Tile = props => (
+//   <div
+//     className={props.class(props.tileObject)}
+//     onClick={() => props.tileClick(props.tileObject)}
+//   >
+//     {props.children}
+//   </div>
+// );
 
 export default Tile;
