e20fea43156c5438f9ce81bb1fb6903eabd3add4
Daland Dixon <dalanddixon@Dalands-MBP.fios-router.home>
2019-04-18 11:43:22 -0400

    Delete commented code

--- client/src/components/Tile/Tile.js
+++ client/src/components/Tile/Tile.js
@@ -10,14 +10,4 @@ const Tile = props => (
   </div>
 );
 
-// ORIGINAL CODE ==================================
-// const Tile = props => (
-//   <div
-//     className={props.class(props.tileObject)}
-//     onClick={() => props.tileClick(props.tileObject)}
-//   >
-//     {props.children}
-//   </div>
-// );
-
 export default Tile;
