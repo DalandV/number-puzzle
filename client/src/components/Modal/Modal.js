8c8a494942538aaaa7173831f37d7560f5784a4f
Daland Dixon <dalanddixon@Dalands-MBP.fios-router.home>
2019-04-19 18:20:09 -0400

    Update modal component

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -10,21 +10,26 @@ import {
 } from "reactstrap";
 
 const ModalBS = props => (
-  <Modal isOpen={props.didUserWin} toggle={props.toggle} centered={true}>
+  <Modal 
+    isOpen={props.didUserWin}
+    toggle={props.toggle}
+    centered={true}
+    >
     <ModalHeader toggle={props.toggle}>You Won!!</ModalHeader>
     <ModalBody>
       Congratulations on completing the puzzle! Add your name below so that you
       can be added to our leaderboard.
       <Form className="mt-2">
         <Input
-          // value={}
+          value={props.name}
+          name="name"
           type="text"
           placeholder="Enter your name here"
-          // onChange={props.handleInputChange}
+          onChange={props.handleInputChange}
         />
         <ModalFooter>
-          <Button color="primary" onClick={props.toggle}>
-            Save changes
+          <Button color="primary" onClick={props.handleFormSubmit}>
+            Add Me!
           </Button>{" "}
           <Button color="secondary" onClick={props.toggle}>
             Cancel
